e5b61e2742e6d6195d8911d2207fc7cc82da4044
Vytautas Jakutis <jakutis@wix.com>
2017-02-07 17:57:37 +0200

    storybook: set button index codes to 5.x from 3.x

--- stories/Button/index.js
+++ stories/Button/index.js
@@ -17,8 +17,8 @@ import ButtonTextLink from './ButtonTextLink';
 import ExampleControlled from './ExampleControlled';
 import ExampleControlledRaw from '!raw!./ExampleControlled';
 
-storiesOf('3. Buttons', module)
-  .add('3.0', () => (
+storiesOf('5. Buttons', module)
+  .add('5.0', () => (
     <div>
       <Markdown source={Readme}/>
 
@@ -30,65 +30,65 @@ storiesOf('3. Buttons', module)
 
     </div>
   ))
-  .add('3.1 Standard', () => (
+  .add('5.1 Standard', () => (
     <div>
-      <h1>3.1 Standard</h1>
+      <h1>5.1 Standard</h1>
       <InteractiveCodeExample title="Customize a <Button/>">
         <ButtonStandard/>
       </InteractiveCodeExample>
     </div>
   ))
-  .add('3.2 White', () => (
+  .add('5.2 White', () => (
     <div>
-      <h1>3.1 White</h1>
+      <h1>5.2 White</h1>
       <InteractiveCodeExample title="Customize a <Button/>">
         <ButtonWhite/>
       </InteractiveCodeExample>
     </div>
   ))
-  .add('3.3 Icon Only', () => (
+  .add('5.3 Icon Only', () => (
     <div>
-      <h1>3.3 Icon Only</h1>
+      <h1>5.3 Icon Only</h1>
       <InteractiveCodeExample title="Customize a <Button/>">
         <ButtonIcon/>
       </InteractiveCodeExample>
     </div>
   ))
-  .add('3.4 Error', () => (
+  .add('5.4 Error', () => (
     <div>
-      <h1>3.4 Error</h1>
+      <h1>5.4 Error</h1>
       <InteractiveCodeExample title="Customize a <Button/>">
         <ButtonError/>
       </InteractiveCodeExample>
     </div>
   ))
-  .add('3.5 Premium', () => (
+  .add('5.5 Premium', () => (
     <div>
-      <h1>3.5 Premium</h1>
+      <h1>5.5 Premium</h1>
       <InteractiveCodeExample title="Customize a <Button/>">
         <ButtonPremium/>
       </InteractiveCodeExample>
     </div>
   ))
-  .add('3.6 Transparent', () => (
+  .add('5.6 Transparent', () => (
     <div>
-      <h1>3.6 Transparent</h1>
+      <h1>5.6 Transparent</h1>
       <InteractiveCodeExample title="Customize a <Button/>">
         <ButtonTransparent/>
       </InteractiveCodeExample>
     </div>
   ))
-  .add('3.7 Close', () => (
+  .add('5.7 Close', () => (
     <div>
-      <h1>3.7 Close</h1>
+      <h1>5.7 Close</h1>
       <InteractiveCodeExample title="Customize a <Button/>">
         <ButtonClose/>
       </InteractiveCodeExample>
     </div>
   ))
-  .add('3.8 Text Link', () => (
+  .add('5.8 Text Link', () => (
     <div>
-      <h1>3.8 Text Link</h1>
+      <h1>5.8 Text Link</h1>
       <InteractiveCodeExample title="Customize a <Button/>">
         <ButtonTextLink/>
       </InteractiveCodeExample>
