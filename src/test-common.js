41c0a6a60c07ea5588e59154aa85793893d8cba4
Lior Belinsky <liorbe@wix.com>
2017-02-15 15:08:02 +0200

    made testkit template faster

--- src/test-common.js
+++ src/test-common.js
@@ -17,15 +17,7 @@ export const testkitFactoryCreator = driverFactory => ({wrapper, dataHook}) => {
 
 // enzyme
 export const enzymeTestkitFactoryCreator = driverFactory => ({wrapper, dataHook}) => {
-  const component = wrapper.findWhere(n => {
-    if (n.props().dataHook) {
-      return false;
-    } else {
-      const tmp = document.implementation.createHTMLDocument();
-      tmp.body.innerHTML = n.html();
-      return tmp.body.children && tmp.body.children[0] && tmp.body.children[0].getAttribute('data-hook') === dataHook;
-    }
-  });
+  const component = wrapper.findWhere(n => !n.props().dataHook && n.html().includes(`data-hook="${dataHook}"`));
   return driverFactory({component: component.node, wrapper});
 };
 
