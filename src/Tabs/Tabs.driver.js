e6d5e11f96506b8007665db8889b57801bcf3460
artur-charcenko <arturc@wix.com>
2017-02-21 11:55:19 +0200

    Fix failing specs

--- src/Tabs/Tabs.driver.js
+++ src/Tabs/Tabs.driver.js
@@ -5,11 +5,12 @@ import Tabs from './Tabs';
 import styles from './Tabs.scss';
 
 const tabsDriverFactory = ({component, wrapper}) => {
+  const getTabs = () => [...component.childNodes[0].childNodes];
   return {
     exists: () => !!component,
-    getTitles: () => [...component.childNodes].map(childNode => childNode.textContent),
-    clickTabAt: index => ReactTestUtils.Simulate.click(component.childNodes[index]),
-    getActiveTabIndex: () => [...component.childNodes].findIndex(childNode => childNode.classList.contains(styles.active)),
+    getTitles: () => getTabs().map(childNode => childNode.textContent),
+    clickTabAt: index => ReactTestUtils.Simulate.click(getTabs()[index]),
+    getActiveTabIndex: () => getTabs().findIndex(childNode => childNode.classList.contains(styles.active)),
     setProps: props => render(<div ref={r => component = r.childNodes[0]}><Tabs {...props}/></div>, wrapper),
     isDefaultType: () => Tabs.tabTypes.every(tabType => !component.classList.contains(styles[tabType])),
     isOfType: type => component.classList.contains(styles[type]),
