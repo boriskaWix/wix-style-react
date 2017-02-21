68d5050faf1205bedbf7b2c465c90c29ebe7fc41
Donatas Dautartas <donatasd@wix.com>
2017-02-21 09:41:01 +0200

    add default storybook

--- stories/Tabs/index.js
+++ stories/Tabs/index.js
@@ -3,10 +3,12 @@ import {storiesOf} from '@kadira/storybook';
 import Markdown from '../utils/Components/Markdown';
 
 import ReadMe from '../../src/Tabs/README.md';
+import ExampleDefault from './ExampleDefault';
 
 storiesOf('6. Navigation', module)
   .add('6.3 Tabs', () => (
     <div>
       <Markdown source={ReadMe}/>
+      <ExampleDefault />
     </div>
   ));
